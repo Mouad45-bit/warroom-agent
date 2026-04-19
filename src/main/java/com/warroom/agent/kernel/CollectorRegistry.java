8a196f6556287800b5af992a89c36342759c67d4
mouad45-bit <mouadfekkak2002@gmail.com>
2026-04-19 22:34:46 +0100

    create CollectorRegistry

---
+++ src/main/java/com/warroom/agent/kernel/CollectorRegistry.java
@@ -0,0 +1,23 @@
+package com.warroom.agent.kernel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Registre des composants supervisés.
+ *
+ * Pour l'instant :
+ * - il n'y a pas encore de collecteur réel ;
+ * - on prépare simplement le point d'extension.
+ *
+ * Plus tard, on pourra ajouter ici :
+ * - new LogCollector(...)
+ * - new SyscallCollector(...)
+ * - new CommandCollector(...)
+ */
+public class CollectorRegistry {
+
+    public List<ManagedComponent> registeredComponents() {
+        return new ArrayList<>();
+    }
+}
